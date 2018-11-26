6e1d664e102e3c5d20a0a751a1735427a6dc60e5
Bin Zhou <binzhouchn@gmail.com>
2018-11-26 10:33:08 +0800

    update 20181126

--- 2_keras_tensorflow/1_入门与进阶/biGRU_keras.py
+++ 2_keras_tensorflow/1_入门与进阶/biGRU_keras.py
@@ -1,17 +1,32 @@
 # coding=utf-8
-import numpy as np
-import pandas as pd
+import os
 import re
-import copy
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import KFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+import sys
+import pandas as pd
+import numpy as np
 import gensim
-from gensim.models.word2vec import Word2Vec
-from keras import Input, Model
-from keras.models import Sequential
-from keras.layers import Embedding, Dense, Conv1D, GlobalMaxPooling1D, Concatenate, Dropout, Activation
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+import tensorflow as tf
+import keras
+from keras.layers import *
+from keras.models import *
+from keras.optimizers import *
+from keras.callbacks import *
+from keras.preprocessing import text, sequence
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import  accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import warnings
+warnings.filterwarnings('ignore')
+import os
+import gc
+import random
+from keras.engine.topology import Layer
 from m1 import BOW
 
 maxlen = 100 # 每句话的固定长度(截断或者补全)
@@ -47,112 +62,35 @@ val_X, val_y = X[val_idx],  np.array(df.label)[val_idx]
 # 之前的comments数据是拼接起来的，159571行以后是测试集
 test = copy.deepcopy(bow.doc2num[159571:])
 
-# 用keras构建textCNN模型 version1（这样写比较好一点）-------------------
-class TextCNN(object):
-    def __init__(self, maxlen, max_features, embedding_dim,
-                 weights=None,
-                 trainable=False,
-                 class_num=1,
-                 last_activation='sigmoid',
-                 optimizer='adam',
-                 loss='binary_crossentropy',
-                 metrics=None,
-                 batch_size=64,
-                 epochs=10,
-                 callbacks=None):
-        self.maxlen = maxlen
-        self.max_features = max_features
-        self.embedding_dim = embedding_dim
-        self.filters = 128,
-        self.weights = weights  # weight list
-        self.trainable = trainable
-        self.class_num = class_num
-        self.last_activation = last_activation
-        self.optimizer = optimizer
-        self.loss = loss
-        self.metrics = metrics  # metric list, calculated at end of a batch, always useless
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.callbacks = callbacks  # callbacks list, e.g., model checkpoint, early stopping, customed epoch-level metric...
+# 用keras构建bigru模型-------------------------------------
+def bi_gru_model(maxlen, embeddings_weight, class_num):
+    content = Input(shape=(maxlen,), dtype='int32')
+    embedding = Embedding(
+        name="word_embedding",
+        input_dim=embeddings_weight.shape[0],
+        weights=[embeddings_weight],
+        output_dim=embeddings_weight.shape[1],
+        trainable=False)
 
-    def fit(self, x_train, y_train, x_val, y_val):
-        input = Input((self.maxlen,))
-        embedding = Embedding(self.max_features, self.embedding_dim,
-                              input_length=self.maxlen,
-                              weights=self.weights,
-                              trainable=self.trainable)(input)
-        convs = []
-        for kernel_size in [3, 4, 5]:
-            c = Conv1D(self.filters, kernel_size, activation='relu')(embedding)
-            c = GlobalMaxPooling1D()(c)
-            convs.append(c)
-        x = Concatenate()(convs)
-        x = Dropout(0.5)(x)
-        output = Dense(self.class_num, activation=self.last_activation)(x)
-        self.model = Model(inputs=input, outputs=output)
+    x = SpatialDropout1D(0.2)(embedding(content))
 
-        self.model.compile(self.optimizer, loss=self.loss, metrics=self.metrics)
+    x = Bidirectional(GRU(200, return_sequences=True))(x)
+    x = Bidirectional(GRU(200, return_sequences=True))(x)
 
-        self.model.fit(x_train, y_train,
-                       validation_data=(x_val, y_val),
-                       batch_size=self.batch_size,
-                       epochs=self.epochs,
-                       callbacks=self.callbacks)
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
 
-        return self.model
+    conc = concatenate([avg_pool, max_pool])
 
-    def predict(self, x):
-        return self.model.predict(x)
+    x = Dropout(0.2)(Activation(activation="relu")(BatchNormalization()(Dense(1000)(conc))))
+    x = Activation(activation="relu")(BatchNormalization()(Dense(500)(x)))
+    output = Dense(class_num, activation="sigmoid")(x)
 
-model = TextCNN(maxlen, embedding_matrix.shape[0], embedding_matrix.shape[1],
-                weights=[embedding_matrix],
-                trainable=False,
-                optimizer='adam',
-                loss='binary_crossentropy',
-                metrics=['accuracy'],
-                batch_size=batch_size,
-                epochs=epochs)
+    model = Model(inputs=content, outputs=output)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-model.fit(train_X, train_y, val_X, val_y)
-
-# 用keras构建textCNN模型 version2------------------------------
-model = Sequential()
-# we start off with an efficient embedding layer which maps
-# our vocab indices into embedding_dims dimensions
-model.add(Embedding(embedding_matrix.shape[0],
-                    embedding_matrix.shape[1],
-                    input_length=maxlen,
-                    weights=[embedding_matrix],
-                    trainable=False))
-model.add(Dropout(0.2))
-
-# we add a Convolution1D, which will learn filters
-# word group filters of size filter_length:
-filters = 128
-kernel_size = 3
-hidden_dims = 250
-model.add(Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1))
-# we use max pooling:
-model.add(GlobalMaxPooling1D())
-
-# We add a vanilla hidden layer:
-model.add(Dense(hidden_dims))
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-
-# We project onto a single unit output layer, and squash it with a sigmoid:
-model.add(Dense(1)) # 二分类问题则这里为1
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-model.fit(train_X, train_y,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(val_X, val_y))
-
+# 训练
+gru_model = bi_gru_model(maxlen, embedding_matrix, 1)
+for i in range(1):
+    gru_model.fit(train_X, train_y, epochs=10, batch_size=128, validation_data=(val_X, val_y))
