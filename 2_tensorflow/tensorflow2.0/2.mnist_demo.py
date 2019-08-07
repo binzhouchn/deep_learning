32f1351a2bbbd4dcdde54574630b26a7b59e3564
binzhouchn <binzhouchn@gmail.com>
2019-08-07 14:41:07 +0800

    update on 20190807

--- 2_tensorflow/tensorflow2.0/2.mnist_demo.py
+++ 2_tensorflow/tensorflow2.0/2.mnist_demo.py
@@ -7,8 +7,6 @@
  1. 自己构造训练和验证循环，和torch很像
  2. with tf.GradientTape() as tape
 '''
-
-
 import os
 import tensorflow as tf
 import numpy as np
@@ -80,3 +78,49 @@ for epoch in range(3):
     val_acc = val_acc_metric.result()
     val_acc_metric.reset_states()
     print('Validation acc: %s' % (float(val_acc),))
+
+#########################################################
+#########################################################
+# 变量求导优化(tf.GradientTape补充，和上面差不多)
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.W = tf.Variable(5., name='weight')
+        self.B = tf.Variable(10., name='bias')
+    def call(self, inputs):
+        return inputs * self.W + self.B
+
+# A toy dataset of points around 3 * x + 2
+NUM_EXAMPLES = 2000
+training_inputs = tf.random.normal([NUM_EXAMPLES])
+noise = tf.random.normal([NUM_EXAMPLES])
+training_outputs = training_inputs * 3 + 2 + noise
+
+# The loss function to be optimized
+def loss(model, inputs, targets):
+    error = model(inputs) - targets
+    return tf.reduce_mean(tf.square(error))
+
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets)
+    return tape.gradient(loss_value, [model.W, model.B])
+
+# Define:
+# 1. A model.
+# 2. Derivatives of a loss function with respect to model parameters.
+# 3. A strategy for updating the variables based on the derivatives.
+model = MyModel()
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+
+print("Initial loss: {:.3f}".format(loss(model, training_inputs, training_outputs)))
+
+# Training loop
+for i in range(300):
+    grads = grad(model, training_inputs, training_outputs)
+    optimizer.apply_gradients(zip(grads, [model.W, model.B]))
+    if i % 20 == 0:
+        print("Loss at step {:03d}: {:.3f}".format(i, loss(model, training_inputs, training_outputs)))
+
+print("Final loss: {:.3f}".format(loss(model, training_inputs, training_outputs)))
+print("W = {}, B = {}".format(model.W.numpy(), model.B.numpy()))
