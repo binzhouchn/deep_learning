622d83df6db76f370dcc83e6b85109e4a7c09fab
binzhouchn <binzhouchn@gmail.com>
2019-08-08 11:44:47 +0800

    update on 20190808

--- 2_tensorflow/tensorflow2.0/5.csv_binary_classify.py
+++ 2_tensorflow/tensorflow2.0/5.csv_binary_classify.py
@@ -18,7 +18,7 @@ URL = 'data/heart.csv'
 dataframe = pd.read_csv(URL)
 dataframe.head()
 
-## 1.数据处理
+######################## 1.数据处理 ########################
 def df_to_dataset(dataframe, shuffle=True, batch_size=32):
     dataframe = dataframe.copy()
     labels = dataframe.pop('target')
@@ -36,3 +36,44 @@ train_ds = df_to_dataset(train, batch_size=batch_size)
 val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
 test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
 
+######################## 2.feature col处理(特征处理) ########################
+feature_columns = []
+
+# numeric cols
+for header in ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'slope', 'ca']:
+    feature_columns.append(feature_column.numeric_column(header))
+
+# bucketized cols
+age_buckets = feature_column.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+feature_columns.append(age_buckets)
+
+# indicator cols
+thal = feature_column.categorical_column_with_vocabulary_list(
+      'thal', ['fixed', 'normal', 'reversible'])
+thal_one_hot = feature_column.indicator_column(thal)
+feature_columns.append(thal_one_hot)
+
+# embedding cols
+thal_embedding = feature_column.embedding_column(thal, dimension=8)
+feature_columns.append(thal_embedding)
+
+# crossed cols
+crossed_feature = feature_column.crossed_column([age_buckets, thal], hash_bucket_size=1000)
+crossed_feature = feature_column.indicator_column(crossed_feature)
+feature_columns.append(crossed_feature)
+
+# 搭建feature layer
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+######################## 3.搭建模型并fit ########################
+model = tf.keras.Sequential([
+    feature_layer,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+             loss='binary_crossentropy',
+             metrics=['accuracy'])
+model.fit(train_ds, validation_data=val_ds,epochs=5)
