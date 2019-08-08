a700571d17688f7ec572ba1ef9561bf330ebbbbe
binzhouchn <binzhouchn@gmail.com>
2019-08-08 10:26:11 +0800

    update on 20190808

--- 2_tensorflow/tensorflow2.0/4.imdb.py
+++ 2_tensorflow/tensorflow2.0/4.imdb.py
@@ -33,3 +33,26 @@ test_x = keras.preprocessing.sequence.pad_sequences(
     test_x, value=word2id['<PAD>'],
     padding='post', maxlen=256
 )
+
+x_val = train_x[:10000]
+x_train = train_x[10000:]
+
+y_val = train_y[:10000]
+y_train = train_y[10000:]
+
+# Prepare the training dataset.
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(512)
+
+val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+val_dataset = val_dataset.batch(512)
+
+# 训练模型
+history = model.fit(train_dataset, epochs=40, validation_data=val_dataset, verbose=1)
+result = model.evaluate(test_x, text_y)
+print(result)
+# 或者
+history = model.fit(x_train,y_train,
+                   epochs=40, batch_size=512,
+                   validation_data=(x_val, y_val),
+                   verbose=1)
