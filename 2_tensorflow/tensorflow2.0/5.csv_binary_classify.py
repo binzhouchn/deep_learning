959f8a022e1e18c6ce8c7aee1c7c28c1c9df38b7
binzhouchn <binzhouchn@gmail.com>
2019-08-08 11:44:03 +0800

    update on 20190808

--- 2_tensorflow/tensorflow2.0/5.csv_binary_classify.py
+++ 2_tensorflow/tensorflow2.0/5.csv_binary_classify.py
@@ -4,3 +4,35 @@
 # Date: 20190808
 # 一般csv文件（包含数值和类别型特征）二分类
 
+import os
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow import feature_column
+import matplotlib.pyplot as plt
+
+URL = 'data/heart.csv'
+dataframe = pd.read_csv(URL)
+dataframe.head()
+
+## 1.数据处理
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('target')
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+
+batch_size = 32
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
