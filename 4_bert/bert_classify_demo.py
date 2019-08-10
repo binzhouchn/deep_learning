0b0e248ffed604b83a183a09eba21ead23b1b8e0
binzhouchn <binzhouchn@gmail.com>
2019-08-10 18:08:53 +0800

    update on 20190810

--- 4_bert/bert_classify_demo.py
+++ 4_bert/bert_classify_demo.py
@@ -3,3 +3,14 @@
 # Author: zhoubin
 # Date: 20190810
 
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+from datetime import datetime
+
+import bert
+from bert import run_classifier
+from bert import optimization
+from bert import tokenization
+
