8e5fac82daf88a994f271efac1ceb3c447cebb28
binzhouchn <binzhouchn@gmail.com>
2019-08-10 18:20:59 +0800

    update on 20190810

--- 4_bert/bert_classify_demo.py
+++ 4_bert/bert_classify_demo.py
@@ -27,3 +27,43 @@ from bert import tokenization
 '''
 
 #########################2. Data Preprocessing#########################
+# Use the InputExample class from BERT's run_classifier code to create examples from the data
+train_InputExamples = train.apply(lambda x: bert.run_classifier.InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this example
+                                                                   text_a = x['sentence'],
+                                                                   text_b = None,
+                                                                   label = x['label']), axis = 1)
+
+test_InputExamples = test.apply(lambda x: bert.run_classifier.InputExample(guid=None,
+                                                                   text_a = x['sentence'],
+                                                                   text_b = None,
+                                                                   label = x['label']), axis = 1)
+
+# 以上代码也可以自己写一个InputExample的类，以下是InputExample源码
+class InputExample(object):
+  """A single training/test example for simple sequence classification."""
+
+  def __init__(self, guid, text_a, text_b=None, label=None):
+    """Constructs a InputExample.
+
+    Args:
+      guid: Unique id for the example.
+      text_a: string. The untokenized text of the first sequence. For single
+        sequence tasks, only this sequence must be specified.
+      text_b: (Optional) string. The untokenized text of the second sequence.
+        Only must be specified for sequence pair tasks.
+      label: (Optional) string. The label of the example. This should be
+        specified for train and dev examples, but not for test examples.
+    """
+    self.guid = guid
+    self.text_a = text_a
+    self.text_b = text_b
+    self.label = label
+train_InputExamples = train.apply(lambda x: InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this example
+                                                           text_a = x['sentence'],
+                                                           text_b = None,
+                                                           label = x['label']), axis = 1)
+
+test_InputExamples = test.apply(lambda x: InputExample(guid=None,
+                                                           text_a = x['sentence'],
+                                                           text_b = None,
+                                                           label = x['label']), axis = 1)
