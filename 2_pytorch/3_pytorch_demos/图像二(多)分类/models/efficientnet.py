054b1c8d91c2ba20d40a45459034de46af7f5f16
binzhouchn <binzhouchn@gmail.com>
2021-02-19 10:50:57 +0800

    添加图片分类模型-byob

--- 2_pytorch/3_pytorch_demos/图像二(多)分类/models/efficientnet.py
+++ 2_pytorch/3_pytorch_demos/图像二(多)分类/models/efficientnet.py
@@ -2,7 +2,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from ..efficient_utils import (
+from ..efficientnet_utils import (
     round_filters,
     round_repeats,
     drop_connect,
