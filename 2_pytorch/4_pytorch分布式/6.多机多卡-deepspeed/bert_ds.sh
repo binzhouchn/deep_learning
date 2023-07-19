507317f30b907a7604b491d95801a9e7043d718f
binzhouchn <binzhouchn@gmail.com>
2023-07-19 17:30:20 +0800

    6.多机多卡-deepspeed

--- 2_pytorch/4_pytorch分布式/6.多机多卡-deepspeed/bert_ds.sh
+++ 2_pytorch/4_pytorch分布式/6.多机多卡-deepspeed/bert_ds.sh
@@ -6,9 +6,12 @@ export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=ALL
 export TORCH_DISTRIBUTED_DEBUG=INFO
 #export NCCL_IB_DISABLE=1
+#export NCCL_NET=IB
 #export NCCL_SOCKET_IFNAME=enp2s0f0
 #export NCCL_SOCKET_IFNAME="^lo,docker,virbr,vmnet,vboxnet,wl,ww,ppp"
-export NCCL_SOCKET_IFNAME=en,eth,em,bond
+export NCCL_SOCKET_IFNAME=en,eth,em,bond,ens
 
-
-deepspeed bert_ds.py --deepspeed_config ds_config.json
+#单机多卡
+#deepspeed bert_ds.py --deepspeed --deepspeed_config ds_config.json
+#多机多卡
+deepspeed --hostfile hostfile --master_addr xx.xxx.72.6 --master_port=29346 --include="xx.xxx.72.6:0,1,2,3@xx.xxx.72.5:0,1,2,3" bert_ds.py --deepspeed --deepspeed_config ds_config.json
---
+++ 2_pytorch/4_pytorch分布式/6.多机多卡-deepspeed/readme.txt
@@ -0,0 +1,4 @@
+和单机多卡的deepspeed差不多
+
+增加hostfile
+然后bert_ds.sh启动的时候用多机多卡的启动方式即可！
